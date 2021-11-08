99882504a6fbc5c1dced5f1c49320af469327861
JoseIgnacioTapia <josignaciot@gmail.com>
2021-11-08 17:20:38 -0300

    Showing Feedback To The User

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -4,6 +4,7 @@ import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
@@ -20,6 +21,7 @@ const AuthForm = () => {
 
     // Add validation here
 
+    setIsLoading(true);
     if (isLogin) {
     } else {
       fetch(
@@ -36,12 +38,17 @@ const AuthForm = () => {
           },
         }
       ).then(res => {
+        setIsLoading(false);
         if (res.ok) {
           // ...
         } else {
           return res.json().then(data => {
-            // show an error modal
+            let errorMessage = 'Authentication failed!';
+            if (data && data.error && data.error.message) {
+              errorMessage = data.error.message;
+            }
             console.log(data);
+            alert(errorMessage);
           });
         }
       });
@@ -66,7 +73,10 @@ const AuthForm = () => {
           />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          {!isLoading && (
+            <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          )}
+          {isLoading && <p>Sending request...</p>}
           <button
             type="button"
             className={classes.toggle}
